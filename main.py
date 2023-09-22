d33fc453d5395d746d2e06afdbcff7fc51012a96
Julia Gabrielle <107364740+juliagbmaciel@users.noreply.github.com>
2023-09-22 17:00:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,98 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from pydantic import BaseModel
-
-cred = credentials.Certificate("cred.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
 
 app = FastAPI()
 
+
+origins = ["*"]  # "*" permite solicitações de qualquer origem. Você pode ajustar isso para permitir apenas origens específicas.
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
-    allow_methods=['*'],
-    allow_headers=['*']
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Isso permite todos os métodos HTTP (GET, POST, PUT, etc.).
+    allow_headers=["*"],  # Isso permite todos os cabeçalhos HTTP.
 )
 
 
-class Restaurant(BaseModel):
-    name: str
-    description: str
-    location: str
+restaurantes = [
+    {
+        "nome": "Jangada Restaurante",
+        "descricao": "Restaurante bom da peste",
+        "local": "Campinas - SP",
+        "avaliacao": 5,
+        "image": "https://www.restaurantejangada.com.br/wp-content/uploads/2019/06/campinas3-1024x683.jpg",
+        "id": 1
+    },
+     {
+        "nome": "Jangada Restaurante 2",
+        "descricao": "Restaurante bom da peste",
+        "local": "Campinas - SP",
+        "avaliacao": 5,
+        "image": "https://www.restaurantejangada.com.br/wp-content/uploads/2019/06/campinas3-1024x683.jpg",
+         "id": 2
+    }
+]
+
+pratos = [
+    {
+        "nome": "Prato tal",
+        "descricao": "desc tal",
+        "preco": 20.00,
+        "id_restaurante": 1
+    },
+    {
+        "nome": "Prato tal",
+        "descricao": "desc tal",
+        "preco": 20.00,
+        "id_restaurante": 2
+    }
+
+]
 
 
-class Options(BaseModel):
-    name: str
-    description: str
-    price: float
-    restaurant_id: str
+@app.get('/restaurants')
+async def get_restaurants():
+    for rest in restaurantes:
+        print(rest)
+    return restaurantes
 
 
-# Rota para criar um novo restaurante
-@app.post("/restaurants/", response_model=Restaurant)
-async def create_restaurant(restaurant: Restaurant):
-    try:
-        restaurant_data = restaurant.dict()
-        restaurant_ref = db.collection("restaurantes").add(restaurant_data)
-        return restaurant_data
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-
-
-@app.get("/restaurants/")
-async def get_all_restaurants():
-    try:
-        restaurantes_ref = db.collection("restaurantes")
-        restaurantes = restaurantes_ref.stream()
-
-
-        lista_de_restaurantes = []
-
-        for restaurante in restaurantes:
-            dados_do_restaurante = restaurante.to_dict()
-            dados_do_restaurante['id'] = restaurante.id
-            lista_de_restaurantes.append(dados_do_restaurante)
-
-        return lista_de_restaurantes
-    except Exception as e:
-        return {"erro": str(e)}
-
-
-@app.get("/restaurants/{restaurant_name}")
-async def get_restaurant_by_name(restaurant_name: str):
-    try:
-        restaurantes_ref = db.collection("restaurantes")
-        query = restaurantes_ref.where("name", "==", restaurant_name)
-        resultados = query.stream()
-
-        for resultado in resultados:
-            return resultado.to_dict()
-
-        raise HTTPException(status_code=404, detail="Restaurante não encontrado")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.get('/restaurant/{id}')
+async def get_restaurant_by_id(id: int):
+    restaurante = ''
+    for rest in restaurantes.items():
+        if rest[0] == id:
+            restaurante = rest
+    return restaurante
 
 
 
-
-@app.post("/options/", response_model=Options)
-async def create_food_option(option: Options):
-    try:
-        option_data = option.model_dump()
-        option_ref = db.collection("opcoes_De_pratos").add(option_data)
-        return option_data
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.get('/food/{id}')
+async def get_food_by_restaurant(id: int):
+    lista_pratos = []
+    for prato in pratos.values():
+        if prato['id_restaurante'] == id:
+            lista_pratos.append(prato)
+    return lista_pratos
 
 
+@app.get('/restaurant/{id_restaurant}/food/{id_food}')
+async def get_food_by_rest_by_idfood(id_restaurant: int, id_food: int):
+    prato_correto = ''
+    for prato in pratos.items():
+        if prato[1]['id_restaurante'] == id_restaurant:
+            if prato[0] == id_food:
+                prato_correto = prato
 
-@app.get('/food_for_restaurant/{id}')
-async def get_foods_by_restaurant(id: str):
-    try:
-        foods_ref = db.collection('opcoes_De_pratos')
-        query = foods_ref.where("restaurant_id", "==", id)
-        resultados = query.stream()
-
-        for resultado in resultados:
-            return resultado.to_dict()
-
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return prato_correto
 
 
 
 
 
 
+if __name__ == "__main__":
+    import uvicorn
 
-
-
-
+    uvicorn.run("main:app", host='127.0.0.1', port=8001, log_level="info", reload=True)
